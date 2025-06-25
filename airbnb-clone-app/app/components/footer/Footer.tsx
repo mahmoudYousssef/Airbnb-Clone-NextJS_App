9f0765267ee739f4f4c95fb63df239274181478f
Mahmoud Youssef <ma7moudyoussef21@gmail.com>
2025-06-25 18:29:08 +0300

    Update Footer

--- airbnb-clone-app/app/components/footer/Footer.tsx
+++ airbnb-clone-app/app/components/footer/Footer.tsx
@@ -1,45 +1,59 @@
+import Link from "next/link";
+
 const Footer = () => {
+  const links = [
+    {
+      title: "ABOUT",
+      links: [
+        "How Airbnb works",
+        "Newsroom",
+        "Investors",
+        "Airbnb Plus",
+        "Airbnb Luxe",
+      ],
+    },
+    {
+      title: "COMMUNITY",
+      links: [
+        "Accessibility",
+        "This is not a real site",
+        "Its a pretty awesome clone",
+        "Referrals accepted",
+        "Papafam",
+      ],
+    },
+    {
+      title: "HOST",
+      links: [
+        "Papa React",
+        "Presents",
+        "Zero to Full Stack Hero",
+        "Hundreds of Students",
+        "Join Now",
+      ],
+    },
+    {
+      title: "SUPPORT",
+      links: [
+        "Help Center",
+        "Trust & Safety",
+        "Say Hi Youtube",
+        "Easer Eggs",
+        "Contact Us",
+      ],
+    },
+  ];
   return (
     <footer>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 text-gray-800">
-        {/* ABOUT Section */}
-        <div className="space-y-4 text-xs text-gray-800">
-          <h5 className="font-bold">ABOUT</h5>
-          <p>How Airbnb works</p>
-          <p>Newsroom</p>
-          <p>Investors</p>
-          <p>Airbnb Plus</p>
-          <p>Airbnb Luxe</p>
-        </div>
-
-        {/* COMMUNITY Section */}
-        <div className="space-y-4 text-xs text-gray-800">
-          <h5 className="font-bold">COMMUNITY</h5>
-          <p>Accessibility</p>
-          <p>This is not a real site</p>
-          <p>Its a pretty awesome clone</p>
-          <p>Referrals accepted</p>
-          <p>Papafam</p>
-        </div>
-
-        {/* HOST Section */}
-        <div className="space-y-4 text-xs text-gray-800">
-          <h5 className="font-bold">HOST</h5>
-          <p>Papa React</p>
-          <p>Presents</p>
-          <p>Zero to Full Stack Hero</p>
-          <p>Hundreds of Students</p>
-          <p>Join Now</p>
-        </div>
-
-        <div className="space-y-4 text-xs text-gray-800">
-          <h5 className="font-bold">SUPPORT</h5>
-          <p>Help Center</p>
-          <p>Trust & Safety</p>
-          <p>Say Hi Youtube</p>
-          <p>Easer Eggs</p>
-          <p>Contact Us</p>
-        </div>
+        {links.map((link) => (
+          <div key={link.title} className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">{link.title}</h5>
+            {link.links.map((item) => (
+              <Link href={item} key={item} className="block">{item}</Link>
+            ))}
+          </div>
+        ))}
       </div>
     </footer>
   );
